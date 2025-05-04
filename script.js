3739e03c93b3d2d2161c8a08f367e08354c183fb
GAUTAMVERM <89487056+GAUTAMVERM@users.noreply.github.com>
2025-05-05 00:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
-
 const output = document.getElementById("output");
 const errorDiv = document.getElementById("error");
 const loadingDiv = document.getElementById("loading");
